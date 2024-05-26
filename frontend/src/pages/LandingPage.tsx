80faf0d027b2996eddd45867f11d4a6cfe6e6d22
AFANDI soufiane <afasou9@gmail.com>
2024-05-26 19:25:39 +0100

    pushing KPIs Management Website

---
+++ frontend/src/pages/LandingPage.tsx
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from "react";
+import { User } from "../types";
+import httpClient from "../httpClient";
+
+const LandingPage: React.FC = () => {
+  const [user, setUser] = useState<User | null>(null);
+  const logoutUser = async () => {
+    await httpClient.post("//localhost:5000/logout");
+    window.location.href = "/welcome";
+  };
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const resp = await httpClient.get("//localhost:5000/@me");
+
+        setUser(resp.data);
+      } catch (error) {
+        console.log("Not authentificated");
+      }
+    })();
+  }, []);
+
+  return (
+    <>
+      <h1> KPI Managements Platform </h1>
+      {user != null ? (
+        <>
+          <h2> Logged in</h2>
+          <p>
+            Welcome {user.id} : {user.email}
+          </p>
+
+          <button onClick={logoutUser}>Log out</button>
+        </>
+      ) : (
+        <div>
+          <h4>You're not logged in</h4>
+          <a href="/login">
+            <button>Login</button>
+          </a>
+          <a href="/register">
+            <button>Register</button>
+          </a>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default LandingPage;
